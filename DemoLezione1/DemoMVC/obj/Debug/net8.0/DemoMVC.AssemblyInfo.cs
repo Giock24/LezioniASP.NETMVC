77e52981437cbf35257cd55e74e4da664b815011
Andrea Fabbri <andrea.fabbri@newteam.it>
2025-11-03 11:18:19 +0100

    Fix commit

--- DemoLezione1/DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoLezione1/DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ef59f17582b35dd628dca731a6d82b831f8a05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc7df016f572ec421da3d01bfad0fdd4e1c57ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
