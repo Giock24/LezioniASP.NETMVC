9eebba7e8913b9b3d81298e95ec3d74bae089624
Fabio Consalici <Fabio.Consalici@newteamsoftware.com>
2025-11-07 16:17:59 +0100

    Inizio gestione DB

---
+++ DemoLezione1/DemoMVC/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using DemoMVC.Core.DTO;
+using DemoMVC.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMVC.Controllers;
+
+public class CategoriesController : Controller
+{
+    private readonly NorthwindContext northwindContext;
+
+    public CategoriesController(NorthwindContext northwindContext)
+    {
+        this.northwindContext = northwindContext;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var categories = await northwindContext.Categories.Select(c => new CategoriaDTO { Id = c.CategoryId,Nome=c.CategoryName,Descrizione=c.Description }).ToListAsync(); //.Include(c=> c.Products).ToList();
+
+        return View(categories);
+    }
+}
