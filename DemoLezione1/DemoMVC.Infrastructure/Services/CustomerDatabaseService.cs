dfdb3c76f66c7cdf6c9fb7fb3c9d1edf1d89d923
Gianluca Consoli <gianluca.consoli@newteam.it>
2025-11-14 16:03:31 +0100

    Creato nuova interfaccia per usare bd

---
+++ DemoLezione1/DemoMVC.Infrastructure/Services/CustomerDatabaseService.cs
@@ -0,0 +1,12 @@
+﻿using DemoMVC.Core.Interfaces;
+using DemoMVC.Core.ViewModel.Customers;
+
+namespace DemoMVC.Infrastructure.Services;
+
+public class CustomerDatabaseService : ICustomerData
+{
+    public Task<CustomerDetailsViewModel> GetCustomerDetails(string customerId)
+    {
+        throw new NotImplementedException();
+    }
+}
