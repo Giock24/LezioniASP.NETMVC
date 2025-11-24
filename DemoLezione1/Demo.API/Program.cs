9406a1a52b0f3c43da2abda2295b7ae6b1d173d8
Fabio Consalici <Fabio.Consalici@newteamsoftware.com>
2025-11-24 18:02:41 +0100

    api crud category

--- DemoLezione1/Demo.API/Program.cs
+++ DemoLezione1/Demo.API/Program.cs
@@ -1,8 +1,11 @@
 using DemoMVC.Core.DTO;
 using DemoMVC.Data.Models;
+using DemoMVC.Infrastructure.RandomUser.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Polly;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -42,6 +45,54 @@ app.MapGet("/categories/{id}", async (NorthwindContext context, int id) => {
          Id = id, Descrizione = category.Description,
          Nome = category.CategoryName, NumeroProdotti =category.Products.Count()
     });
+});
+
+app.MapPost("/categories/", async (NorthwindContext context, CategoriaCreaDTO nuovaCategoria) => {
+    var category = new Category { CategoryName = nuovaCategoria.Nome, Description = nuovaCategoria.Descrizione };
+    context.Categories.Add(category);
+    await context.SaveChangesAsync();
+    return Results.Created($"/categories/{category.CategoryId}", new CategoriaDTO
+    {
+        Id = category.CategoryId,
+        Descrizione = category.Description,
+        Nome = category.CategoryName,
+        NumeroProdotti = category.Products.Count()
+    });
+});
+
+app.MapDelete("/categories/{id}", async(NorthwindContext context,int id) => {
+    var categoryToDelete = await context.Categories.FindAsync(id);
+    if (categoryToDelete is null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        context.Categories.Remove(categoryToDelete);
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    }
+} );
+app.MapPut("/categories/{id}", async (NorthwindContext context, CategoriaModificaDTO categoriaModificata, int id) =>
+{
+    var categoryToEdit = await context.Categories.FindAsync(id);
+    if (categoryToEdit is null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        if (!String.IsNullOrEmpty(categoriaModificata.Descrizione))
+        {
+            categoryToEdit.Description = categoriaModificata.Descrizione;
+        }
+        if (!String.IsNullOrEmpty(categoriaModificata.Nome))
+        {
+            categoryToEdit.CategoryName = categoriaModificata.Nome;
+        }
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    }
 });
 
 
